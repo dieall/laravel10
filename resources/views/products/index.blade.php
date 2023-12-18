f226d9a4cd22fc0b3d795b860715378383e43f9a
AA_152022253_Aldi <aldi@mhs.itenas.ac.id>
2023-12-18 16:13:01 +0700

    Membuat data barang dan user

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('contents')
+
+html
+@endsection
---
+++ resources/views/superadmin/index.blade.php
@@ -0,0 +1,21 @@
+<!-- resources/views/superadmin/index.blade.php -->
+
+@include('layout.app')
+
+@section('title', 'Superadmin Dashboard')
+
+@section('content')
+    <!-- Content specific to superadmin dashboard -->
+    <h1>Welcome</h1>
+
+    <!-- Additional superadmin content -->
+
+    <div>
+        <!-- Example: Display a list of superadmin-specific data -->
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </div>
+@endsection
